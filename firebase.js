53c7fc774302d5291ef1a8454f43112485091fbd
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-22 18:04:03 -0300

    add doc, getDoc, updateDoc function

--- firebase.js
+++ firebase.js
@@ -7,7 +7,9 @@ import {
   getDocs,
   onSnapshot,
   deleteDoc,
-  doc
+  doc,
+  getDoc,
+  updateDoc,
 } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -27,14 +29,20 @@ const app = initializeApp(firebaseConfig);
 
 const db = getFirestore();
 
-export const saveTask = (title, description) => 
-  addDoc(collection(db, 'tasks'), {
+export const saveTask = (title, description) =>
+  addDoc(collection(db, "tasks"), {
     title,
-    description
-  })
+    description,
+  });
 
-export const getTasks = () => getDocs(collection(db, 'tasks'))
+export const getTasks = () => getDocs(collection(db, "tasks")); //obtener tareas en Plural
 
-export const onGetTasks = (callback) => onSnapshot(collection(db, 'tasks'), callback)
+export const onGetTasks = (callback) =>
+  onSnapshot(collection(db, "tasks"), callback);
 
-export const deleteTask = id => deleteDoc(doc (db, 'tasks', id))
+export const deleteTask = (id) => deleteDoc(doc(db, "tasks", id));
+
+export const getTask = (id) => getDoc(doc(db, "tasks", id));
+
+export const updateTask = (id, newFields) =>
+  updateDoc(doc(db, "tasks", id), newFields);
