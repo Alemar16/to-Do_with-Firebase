bab7b8c0a6715093e124fc995defc3bf978468f8
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-22 12:35:35 -0300

    add de functions firebase

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
-import {} from "https://www.gstatic.com/firebasejs/10.2.0/firebase-firestore.js";
+import  {collection, getFirestore, addDoc } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,3 +16,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+export const saveTask = (title, description) => 
+  addDoc(collection(db, 'tasks'), {
+    title,
+    description
+  })
+
