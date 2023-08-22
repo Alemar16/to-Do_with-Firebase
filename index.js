340ed3a37545d966f2508f74c96acc895c791a95
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-22 18:05:01 -0300

    se agrega logica para edita y acualiza cambios

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-import { saveTask, getTasks, onGetTasks, deleteTask } from "./firebase.js";
+import { saveTask, getTasks, onGetTasks, deleteTask, getTask, updateTask } from "./firebase.js";
 
 const taskForm = document.getElementById("task-form");
 const taskContainer = document.getElementById("task-container");
+
+let editStatus = false;
+let id = "";
 
 window.addEventListener("DOMContentLoaded", async () => {
   onGetTasks((querySnapshot) => {
@@ -13,17 +16,34 @@ window.addEventListener("DOMContentLoaded", async () => {
       <h3>${task.title}</h3>
       <p>${task.description}</p>
       <button class="btn-delete" data-id="${doc.id}">Delete</button>
+      <button class="btn-edit" data-id="${doc.id}">Edit</button>
+
 
     </div>`;
     });
     taskContainer.innerHTML = html;
-    const btnDelete = taskContainer.querySelectorAll(".btn-delete");
+    const btnsDelete = taskContainer.querySelectorAll(".btn-delete");
 
-    btnDelete.forEach((btn) => {
+    btnsDelete.forEach(btn => {
       btn.addEventListener("click", ({ target: { dataset } }) => {
         deleteTask(dataset.id);
       });
     });
+
+    const btnsEdit = taskContainer.querySelectorAll(".btn-edit");
+    btnsEdit.forEach(btn => {
+      btn.addEventListener("click", async ({ target: { dataset } }) => {
+        const doc = await getTask(dataset.id);
+        const task = doc.data();
+        taskForm["task-title"].value = task.title;
+        taskForm["task-description"].value = task.description;
+
+        editStatus = true;
+        id = doc.id;
+        
+        taskForm["btn-task-save"].innerText = "Update";
+      });
+    })
   });
 });
 
@@ -33,7 +53,15 @@ taskForm.addEventListener("submit", (e) => {
   const title = taskForm["task-title"];
   const description = taskForm["task-description"];
 
-  saveTask(title.value, description.value);
+  if (!editStatus) {
+    saveTask(title.value, description.value);
+  } else {
+    updateTask(id, { title: title.value, description: description.value });
+
+    editStatus = false;
+    
+  }
+
   taskForm.reset();
 });
 
